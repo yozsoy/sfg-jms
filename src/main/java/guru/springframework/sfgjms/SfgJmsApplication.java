389dec1b2d50f5298110774aa572f7f05a4f4be5
y00562359 <ylozsoy@gmail.com>
2020-04-21 16:02:43 +0300

    461) Using local Active MQ with spring boot

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,8 +1,5 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,14 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgJmsApplication {
 
     public static void main(String[] args) throws Exception {
+// run this and use local Acvive MQ broker in docker
+//docker run -it --rm -e ARTEMIS_USERNAME=myuser -e ARTEMIS_PASSWORD=otherpassword vromero/activemq-artemis
 
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));//configuration from their documentation for the embedded server
-
-        server.start();
+//        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//                .setPersistenceEnabled(false)
+//                .setJournalDirectory("target/data/journal")
+//                .setSecurityEnabled(false)
+//                .addAcceptorConfiguration("invm", "vm://0"));//configuration from their documentation for the embedded server
+//
+//        server.start();
 
         SpringApplication.run(SfgJmsApplication.class, args);
     }
